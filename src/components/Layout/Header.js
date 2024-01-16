23b1a29e01636cc02df788e4e48302ed7e5211b9
abhi9871 <at719273@gmail.com>
2024-01-16 14:06:56 +0530

    Building the header of your restaurant app

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,28 @@
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import React from 'react';
+import HeaderCartButton from './HeaderCartButton';
+import headerImage from '../../assets/meals.jpg';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <React.Fragment>
+    <Navbar expand="lg" className="header z-10 sticky-top top-0 left-0 p-3 w-100">
+      <Container>
+        <Navbar.Brand href="#home" className='fw-bold text-white'>Foodie Point</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <HeaderCartButton />
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <div className='main-image'>
+    <img src={headerImage} alt='A table full of delicious food!'></img>
+    </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
