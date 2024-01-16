bb2ac64de2936c93271e2a340cf58b09f5793635
abhi9871 <at719273@gmail.com>
2024-01-16 18:03:13 +0530

    Adding a Form

--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -7,12 +7,25 @@ const MealsList = (props) => {
       <Card className="meals-list w-75">
         {props.items.map((meal) => {
           return (
-            <CardBody>
+            <div key={meal.id}>
+            <CardBody className="d-flex align-items-center">
+            <div className="items-details">
               <div className="fw-bold">{meal.name} </div>
               <div className="fst-italic">{meal.description}</div>
               <div className="price fw-bold">${meal.price}</div>
-              <hr />
+            </div>
+            <div className="quantity-form d-flex flex-column">
+                <div className="add-order mb-1">
+                <label htmlFor='quantity' className="fw-bold">Amount</label>
+                <input type='text' id="quantity" className="ms-2" />
+                </div>
+                <div className="d-flex justify-content-end">
+                <button type="button" className="btn rounded-5 text-white fw-bold px-4">+Add</button>
+                </div>
+            </div>
             </CardBody>
+            <hr className="mx-3" />
+            </div>
           );
         })}
       </Card>
